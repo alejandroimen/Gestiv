f76ef8e8ed4fa944dbc7d40363356c298790f751
alejandroimen <149030059+alejandroimen@users.noreply.github.com>
2024-05-27 09:45:40 -0600

    Update dependencies.js

--- src/controllers/dependencies.js
+++ src/controllers/dependencies.js
@@ -6,7 +6,7 @@ let bst = new BST();
 let producto1 = new Producto("Led", 20, 5, "Emiten luz cuando se aplica una corriente eléctrica.");
 let producto2 = new Producto("Diodo infrarrojo", 23, 10, "Mandan señales a través de Infrarrojo.");
 let producto3 = new Producto("Diodo puente", 15, 15, "Convierte corriente alterna (CA) en corriente continua (CC).");
-let Producto4 = new Producto("Relevador", 20, 15, "Regula circuitos de alta potencia.")
+let producto4 = new Producto("Relevador", 20, 15, "Regula circuitos de alta potencia.")
 let producto5 = new Producto("Puente H", 31, 53, "Controla el sentido de giro de un motor (CC).");
 let producto6 = new Producto("Terminales", 10, 3, "Permite la unión de cables.");
 let producto7 = new Producto("Resistencia", 62, 1, "Limita el flujo de corriente eléctrica en un circuito.");
@@ -17,7 +17,7 @@ let producto10 = new Producto ("Fusibles", 12, 7, "Interrumpen el flujo de corri
 bst.add(producto1);
 bst.add(producto2);
 bst.add(producto3);
-bst.add(Producto4);
+bst.add(producto4);
 bst.add(producto5);
 bst.add(producto6);
 bst.add(producto7);
